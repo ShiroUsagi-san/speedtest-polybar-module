885a23b9bcc5f24da35c52c8d08400c0ec2ef438
white rabbit <whiterabbit@peacefulnukes.com>
2017-11-05 18:40:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Speedtest
+
+![screenshoot](screenshoot.png)
+
+##Dependencies
+[Speedtest-cli](https://github.com/sivel/speedtest-cli)
+[Awesome-font](http://fontawesome.io/)
+
+##Setup
+`cd ~/.config/polybar`
+`git clone https://github.com/ShiroUsagi-san/speedtest-polybar-module`
+
+##Module
+```Bash
+[module/speedtest]
+type = custom/script
+exec = echo 'waiting for info...   ' & ~/.config/polybar/speedtest_module/speedtest_api.py
+tail = true
+interval = 3600
+```
---
+++ speedtest_api.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 white <teo.nespoulet@gmail.com>
+#
+# Distributed under terms of the MIT license.
+import speedtest
+s = speedtest.Speedtest()
+s.get_best_server()
+s.download()
+s.upload()
+result_doct = s.results.dict()
+dl_converter = float(result_doct['download'] / 1000000)
+up_converter = float(result_doct['upload']   / 1000000)
+print('\uf0ab {0:.3f} Mo \uf0aa {1:.3f} Mo \uf111 {2:.0f}   '.format(dl_converter,up_converter, result_doct['ping']))
